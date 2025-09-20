73e66a30503455044289490c3559dfb96f7e58f3
shivin289 <nk7085278@gmail.com>
2025-09-20 18:14:50 +0530

    chatbot

---
+++ src/config/chatbot.js
@@ -0,0 +1,34 @@
+        
+ import { GoogleGenerativeAI } from "@google/generative-ai";
+         
+              
+  const apiKey = import.meta.env.VITE_GEMINI_API_KEY;
+   const genAI = new GoogleGenerativeAI(apiKey);
+   console.log("enter your api key");
+              
+const model = genAI.getGenerativeModel({
+  model: "gemini-2.0-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 40,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function run(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+    history: [
+    ],
+  });
+
+  const result = await chatSession.sendMessage(prompt);
+  const response=result.response;
+  console.log(response.text());
+  return response.text();
+}
+
+export default run;
